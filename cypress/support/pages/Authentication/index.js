e27a9bcef0f236e32ef6c74a777f574639faceb5
Ricardo Oliveira <ricardooliveiraadvpb@gmail.com>
2022-05-15 21:36:59 -0300

    Ajustando função que cria e-mail aleatório

--- cypress/support/pages/Authentication/index.js
+++ cypress/support/pages/Authentication/index.js
@@ -4,7 +4,6 @@ const el = require("./elements").ELEMENTS;
 
 class Authentication {
     inserirEmail(){
-        //cy.get(el.email).type("jrturismo@mail.com");
         const randomString = Math.random().toString(36).replace(/[^a-z]+/g,'').substring(0,5);
         const randomMail = randomString + "@mail.com"
         cy.get(el.email).type(randomMail);
